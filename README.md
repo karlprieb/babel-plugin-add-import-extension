8664837de729df56205faa9b575f529d4c1cc73f
Karl Prieb <karl@prieb.me>
2019-10-08 16:26:30 -0300

    check for node_modules and directories

--- README.md
+++ README.md
@@ -31,8 +31,8 @@ will be converted to:
 import { add, double } from './lib/numbers.js'
 ```
 
-Remember that this plugin will not override extensions and will only add the choosed extension, so if your imports already has extensions it will add another one because this plugin is really dummy :)
+Remember that this plugin will not override extensions.
 
-What this plugin does is to compare all imported modules with your `package.json` and if your module is not on your `package.json` it will consider that is a project/local module and add the choosed extension, so for node modules it don't add any extension.
+What this plugin does is to check all imported modules and if your module is not on `node_module` it will consider that is a project/local module and add the choosed extension, so for node modules it don't add any extension.
 
 I made this plugin for personal use on my [typescript boilerplate](https://github.com/karlprieb/typescript-web-starter), but if a lot of people start to use I can add tests and some documentation ;)
--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,29 @@
 const { declare } = require('@babel/helper-plugin-utils')
-const { types: t } = require('@babel/core')
-const path = require('path')
-const packageJson = require(path.resolve('package.json'))
+const { types: { importDeclaration, stringLiteral }} = require('@babel/core')
+const { lstatSync } = require('fs')
+const { resolve, extname, dirname } = require('path')
 
-const dependencies = packageJson.dependencies ? Object.keys(packageJson.dependencies) : []
-const devDependencies = packageJson.devDependencies ? Object.keys(packageJson.devDependencies) : []
-const peerDependencies = packageJson.peerDependencies ? Object.keys(packageJson.peerDependencies) : []
-const optionalDependencies = packageJson.optionalDependencies ? Object.keys(packageJson.optionalDependencies) : []
+const isNodeModule = module => {
+  try {
+    require.resolve(module)
+    return true
+  } catch (e) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+      return false
+    }
+    console.error(e)
+  }
+}
 
-const nodeModules = [...dependencies, ...devDependencies, ...peerDependencies, ...optionalDependencies]
+const isDirectory = source => {
+  try {
+    return lstatSync(source).isDirectory()
+  } catch (e) {
+    return false
+  }
+}
+
+const skipModule = module => isNodeModule(module) || extname(module).length > 0
 
 module.exports = declare((api, options) => {
   api.assertVersion(7)
@@ -18,12 +33,22 @@ module.exports = declare((api, options) => {
   return {
     name: 'add-import-extension',
     visitor: {
-      ImportDeclaration(path) {
+      ImportDeclaration(path, state) {
         const module = path.node.source.value
-        const isNodeModule = nodeModules.some(key => key === module)
 
-        if (!isNodeModule) {
-          path.node.source.value = `${module}.${extension}`
+        if (skipModule(module)) {
+          return
+        }
+
+        const { filename, cwd } = state.file.opts
+        const dir = dirname(filename)
+
+        if (module[0] === '.') {
+          if (isDirectory(resolve(dir, module))) {
+            path.replaceWith(importDeclaration(path.node.specifiers, stringLiteral(`${module}/index.${extension}`)))
+          } else {
+            path.replaceWith(importDeclaration(path.node.specifiers, stringLiteral(`${module}.${extension}`)))
+          }
         }
       },
     },
