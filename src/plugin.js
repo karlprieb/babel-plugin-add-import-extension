f90d731618e755633e643f6bf2fb1047bff60862
Banou26 <me@banou.dev>
2020-01-18 23:46:43 +0100

    refactor: ♻️re-use code

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,6 @@
 const { declare } = require('@babel/helper-plugin-utils')
 const { types: { importDeclaration, exportNamedDeclaration, exportAllDeclaration, stringLiteral }} = require('@babel/core')
-const { lstatSync } = require('fs')
+const { existsSync, lstatSync } = require('fs')
 const { resolve, extname, dirname } = require('path')
 
 const isNodeModule = module => {
@@ -15,76 +15,61 @@ const isNodeModule = module => {
   }
 }
 
-const isDirectory = source => {
-  try {
-    return lstatSync(source).isDirectory()
-  } catch (e) {
-    return false
-  }
-}
+const skipModule = (module, { replace, extension }) =>
+  isNodeModule(module)
+  || replace
+    ? extname(module) === `.${extension}`
+    : extname(module).length
 
-const skipModule = module => isNodeModule(module) || extname(module).length > 0
+const makeDeclaration =
+  ({ declaration, args, replace = false, extension = 'js' }) =>
+      (path, { file: { opts: { filename } }}) => {
+        const { node: { source } } = path
+
+        if (!source || skipModule(source && source.value, { replace, extension })) return
+        const { value: module } = source
+
+        if (module[0] === '.') {
+          const dirPath = resolve(dirname(filename), module)
+          if (existsSync(dirPath) && lstatSync(dirPath).isDirectory()) {
+            path.replaceWith(
+              declaration(
+                ...args(path),
+                stringLiteral(`${module}/index.${extension}`)
+              )
+            )
+          } else {
+            path.replaceWith(
+              declaration(
+                ...args(path),
+                stringLiteral(`${module.slice(0,-extname(module).length)}.${extension}`)
+              )
+            )
+          }
+        }
+    }
 
 module.exports = declare((api, options) => {
   api.assertVersion(7)
 
-  const extension = options.extension ? options.extension : 'js'
-
   return {
     name: 'add-import-extension',
     visitor: {
-      ImportDeclaration(path, state) {
-        if (!path.node.source) { return }
-
-        const module = path.node.source.value;
-
-        if (skipModule(module)) { return }
-
-        const { filename } = state.file.opts
-        const dir = dirname(filename)
-
-        if (module[0] === '.') {
-          if (isDirectory(resolve(dir, module))) {
-            path.replaceWith(importDeclaration(path.node.specifiers, stringLiteral(`${module}/index.${extension}`)))
-          } else {
-            path.replaceWith(importDeclaration(path.node.specifiers, stringLiteral(`${module}.${extension}`)))
-          }
-        }
-      },
-      ExportNamedDeclaration(path, state) {
-        if (!path.node.source) { return }
-
-        const module = path.node.source.value;
-
-        if (skipModule(module)) { return }
-
-        const { filename } = state.file.opts
-        const dir = dirname(filename)
-        if (module[0] === '.') {
-          if (isDirectory(resolve(dir, module))) {
-            path.replaceWith(exportNamedDeclaration(path.node.declaration, path.node.specifiers, stringLiteral(`${module}/index.${extension}`)))
-          } else {
-            path.replaceWith(exportNamedDeclaration(path.node.declaration, path.node.specifiers, stringLiteral(`${module}.${extension}`)))
-          }
-        }
-      },
-      ExportAllDeclaration(path, state) {
-        if (!path.node.source) { return }
-
-        const module = path.node.source.value;
-
-        if (skipModule(module)) { return }
-
-        const { filename } = state.file.opts
-        const dir = dirname(filename)
-        if (module[0] === '.') {
-          if (isDirectory(resolve(dir, module))) {
-            path.replaceWith(exportAllDeclaration(stringLiteral(`${module}/index.${extension}`)))
-          } else {
-            path.replaceWith(exportAllDeclaration(stringLiteral(`${module}.${extension}`)))
-          }
-        }
-      }
+      ImportDeclaration: makeDeclaration({
+        ...options,
+        declaration: importDeclaration,
+        args: ({ node: { specifiers } }) => [specifiers]
+      }),
+      ExportNamedDeclaration: makeDeclaration({
+        ...options,
+        declaration: exportNamedDeclaration,
+        args: ({ node: { declaration, specifiers } }) => [declaration, specifiers]
+      }),
+      ExportAllDeclaration: makeDeclaration({
+        ...options,
+        declaration: exportAllDeclaration,
+        args: () => []
+      })
     }
   }
 })
